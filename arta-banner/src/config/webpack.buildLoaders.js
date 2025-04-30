31e7589cc5476a8d7f45d451295bf6d0e06de26d
rasshivalov <nikita_rasshivalov@mail.ru>
2025-04-30 15:25:07 +0300

    remove font

--- arta-banner/src/config/webpack.buildLoaders.js
+++ arta-banner/src/config/webpack.buildLoaders.js
@@ -19,15 +19,5 @@ export function buildLoaders() {
       test: /\.html$/i,
       use: ["html-loader"],
     },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: "asset/resource",
-      use: {
-        loader: "url-loader",
-      },
-      generator: {
-        filename: "assets/fonts/[name][ext]",
-      },
-    },
   ];
 }
